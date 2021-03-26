265327bed589a0ee144f8261c509716a80f7faf4
Cule219 <culaficstefan@gmail.com>
2021-03-25 22:05:01 -0400

    bcrypt

---
+++ bcrypt.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcryptjs");
+
+// const salt = 10;
+
+const myPassword = "password1";
+
+// const hashedPassword = bcrypt.hashSync(myPassword, salt);
+
+// console.log(hashedPassword);
+
+// bcrypt
+//   .compare("password2", hashedPassword)
+//   .then((result) => {
+//     console.log(result);
+//   });
+
+// for (let salt = 10; salt < 19; salt++) {
+//   console.time(`Time of the hash ${salt}`);
+//   bcrypt.hashSync(myPassword, salt);
+//   console.timeEnd(`Time of the hash ${salt}`);
+// }
+
+console.log(console.Console);
